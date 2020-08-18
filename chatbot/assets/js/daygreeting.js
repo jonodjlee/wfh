f38a7fd57823a3254035d1c2cbb5c9db3d46f702
Jonathan Lee <jonodjlee.github@gmail.com>
2020-08-19 00:18:27 +1000

    perfected ajax and swapped endpoint to formcake

---
+++ chatbot/assets/js/daygreeting.js
@@ -0,0 +1,12 @@
+var d = new Date();
+var weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+var n = weekday[d.getDay()];
+document.getElementById("daygreeting").innerHTML = n;
